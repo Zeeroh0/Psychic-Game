6543ac52f53e220b096046a020778c359f346acb
jtmPC <jon87tyler@gmail.com>
2017-08-05 20:04:45 -0700

    Cleaned up code. Removed stuff that wasn't working.  Added clearer notes on index.html and javascript.js. Added a bonus for when wins=5. Should be ready to go.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,11 @@
       
 $(document).ready(function() {
-  
+    //*******************************//
+    //DECLARE VALIABLES
+    //*******************************//
+
     // This array will hold what the player guesses
-    var guessedLetters = ["a"];
+    var guessedLetters = [];
 
     // This variable will be randomly assigned one of the 26 letters in var letters
     var alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -19,72 +22,75 @@ $(document).ready(function() {
 
     var playerGuess = null;
 
-    // function reset () {
-    //     var guessesLeft = 9;
-    //     guessedLetters = [];
-    //     var letterToGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-    //     console.log("Next letter: " + letterToGuess)
-    //     };
 
-    // if (guessedLetters.indexOf(playerGuess) < 0 && alphabet.indexOf(playerGuess) >= 0) {
-    //         guessedLetters[guessedLetters.length] = playerGuess;
-    //         guessesLeft--;
-    //     }
-
+    //*******************************
+    //DO SOMETHING WITH THOSE VARIABLES
+    //*******************************
 
     //Creat onkeyup event function for when the player presses their guesses
     document.onkeyup = function(event) {
         // Lowercase the letter
         var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        // We'll check if there's a match.
         console.log(playerGuess);
 
+        // We'll check if there's a match.
         if (letterToGuess === playerGuess) {
-            // If there is a match, then player wins and we'll update the HTML to display the win.
+            // If there is a match, tally a win,
             wins++; 
+            //reset the board,
             console.log("You win!");
             guessesLeft = 9;
             guessedLetters = [];
             letterToGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-            console.log("Next letter: " + letterToGuess);                     
+            console.log("Next letter: " + letterToGuess);      
+            //and let the player know they rock.                
             alert("Well done! Perhaps you have the gift...care to try again?");
         } 
+        //But if there isn't a match...
         else {
+            //add the guessed letters to the guessedLetters array 
             guessedLetters.push(playerGuess);
+            //and lower the remaining guess allowance.
             guessesLeft--;
         } 
 
-         
+        //if the player's remaining guesses hits zero...
          if (guessesLeft === 0) {
+            //tally a loss,
             losses++;
+            //reset the board,
             guessesLeft = 9;
             guessedLetters = [];
             letterToGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
             console.log("Next letter: " + letterToGuess);  
+            //and tell the player they suck. 
             alert("Not quite.  Really try to focus this time!");                   
         }
 
-        // if (wins === 1) {
-        //     alert("You are AMAZING!!");
-        //     document.getElementByClass(".winner").innerHTML('<img src="assets/images/winner.jpeg" style="width: 100%;">');
-        // }
+        //If the player gets enough wins,
+        if (wins === 5) {
+            //then praise the player, 
+            alert("You are AMAZING!!");
+            //and troll the player. 
+            document.getElementById("winner").innerHTML ='<img src="assets/images/winner.jpeg" style="width: 100%;">';
+        }
 
 
-        var html =  
+        //Keeping track of stats
+        var newStats =  
         "<p><h3>Guessed so far: " + guessedLetters + "</h3></p>" + 
         "<p><h3>Remaining guesses: " + guessesLeft + "</h3></p>" + 
         "<p><h3>Wins: " + wins + "</h3></p>" + 
         "<p><h3>Losses: " + losses + "</h3></p>";
-        "<p><h3>Guess me: " + letterToGuess + "</h3></p>";
 
-        // place html into the game ID
-        document.querySelector(".stats").innerHTML = html;
+        document.querySelector(".stats").innerHTML = newStats;
         
     }
+
+
 })
 
 // Need to do:
-//have the computer redo its choice
-//reset the board after a win/loss
-//Stats need to not be addative 
+//Make the user's guesses only letters
+//Don't penalize the player for guessing the same letter in the same round
 
